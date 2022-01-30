31825e3d48825663157b28ef5b85a28d5bac6f13
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-01-29 19:07:43 -0800

    created variables and started set up calendar, created html branch

---
+++ script.js
@@ -0,0 +1,18 @@
+//created varibles
+let saveButton = $('')
+let deleteButton = $('')
+let savedTasks= [];
+
+//created calendar 
+$(document).ready(function () {
+    let calendarDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+        $('currentDay').text('Today is' + calendarDay);
+})
+
+
+//created time blocks
+
+//setup let saveButton = $('')
+
+//setup let deleteButton = $('')
