4a1bb196e1e2416901a8da10e5b7afd48326eefb
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-01-30 19:49:38 -0800

    finished js and readme files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Work-Day-Scheduler-
 Usage of 3rd party API 
+
+This is a website that will let you plan your work day out from 9am-5pm, also displays current time and date at the top of the page
+
+I created this website using bootstrap and jquery api
+
+<a href="https://justynsubrai.github.io/Work-Day-Scheduler-/">https://justynsubrai.github.io/Work-Day-Scheduler-/</a>
+
+<img src='work-day-scheduler sc.png'>
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //created varibles
-let saveBtn = $('.saveButton')
-let deleteBtn = $('.deleteButton')
+let saveBtn = $('.saveButton');
+let deleteBtn = $('.deleteButton');
 let savedTasks= [];
 
 //created calendar 
 $(document).ready(function () {
-    var calendarDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+    let calendarDay = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-    $('#currentDay').text('Today is' + calendarDay);
+    $('#currentDay').text('Today is ' + calendarDay);
 })
 
 //created color coded time blocks
@@ -29,5 +29,56 @@ function currentHour () {
 }
 
 //setup let saveButton = $('')
+$(saveBtn).on('click', function (event) {
+    event.preventDefault();
+    let savedItems = $(this).siblings('.col-9').val();
+    let time = $(this).siblings('.time-value').text();
+    let timeObj = {
+        hours: time,
+        task: savedItems};
+    console.log(savedItems);
+    console.log(time);
 
+    localStorage.setItem('saved-tasks', JSON.stringify(savedTasks))
+
+    console.log(time);
+    console.log(savedItems);
+    console.log(timeObj);
+    console.log(savedTasks);
+    
+    for (i = 0; i < savedTasks.length; i++) {
+        retrievedData = JSON.parse(localStorage.getItem('saved-tasks'));
+        savedItems.innerHTML = savedTasks[i].task;
+    }
+})
 //setup let deleteButton = $('')
+$(deleteBtn).on('click', function (event) {
+    event.preventDefault();
+    console.log(savedTasks);
+    for (i = 0; i < savedTasks.length; i++) {
+        localStorage.removeItem('saved-tasks');
+        console.log(savedTasks[i].task)}
+    $(this).siblings('.col-9').val('');
+    console.log($(this).siblings());
+})
+
+$(window).on ('load', function () {
+    console.log(JSON.parse(localStorage.getItem('saved-tasks')));
+    let saved = JSON.parse(localStorage.getItem('saved-tasks'));
+    console.log(saved);
+    let taskField = $('.col-9');
+    let dayHours = $('.time-value');
+    if (saved) {
+        for (i = 0; i < saved.length; i++) {
+            savedTasks.push(saved);
+            console.log(dayHours);
+        if (dayHours[i].innerHTML === saved[i].hours){
+            $(taskField[i]).text(saved[i].task);
+        } else {
+            $(taskField[i]).text();
+        }
+
+        }
+    }
+    currentHour();
+});
